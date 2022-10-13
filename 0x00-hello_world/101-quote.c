627a3d634355b0faed95a3af71ea3e3db15c6e86
Ezeonwuka Kevin <kevinkimon@gmail.com>
2022-10-13 08:05:28 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,7 +13,37 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	int fd;
+	char buf[60];
+	
+	fd = open("myfile.txt", O_CREAT, O_WRONLY, 0600);
+	
+	if(fd == -1)
+	{
+		printf("Failed to create and open file.\n");
+		exit(1);
+	}
+	write(fd, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+	close (fd);
+
+	/* Read */
+
+	fd = open("myfile.txt", O_RDONLY);
+
+	if(fd == -1)                                                                                                                                                    
+        {                                                                                                                                                               
+                printf("Failed to open and read file.\n");                                                                                                            
+                exit(1);                                                                                                                                                
+        }              
+
+	read(fd, buf, 59);
+	buf[13] = '\0';
+
+	close (fd);
+
+	printf("buf: %s\n", buf);
+
 	return (1);
 }
